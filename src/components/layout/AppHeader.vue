252cb9bcd2e97718746a207e7ad1bd483bc2b661
Przemek Nowak <przemek.nowak.pl@gmail.com>
2018-07-16 08:30:48 +0200

    Header improvements

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -6,7 +6,7 @@
     <div class="container">
       <div class="navbar-brand">
 
-        <a class="navbar-item has-background-primary has-text-white has-text-weight-bold" href="/#">
+        <a class="navbar-item has-background-primary has-text-weight-bold" href="/#">
           GITHUB STARS
           <!--<img src="../assets/logo.png">-->
         </a>
@@ -20,20 +20,19 @@
 
       <div class="navbar-menu">
         <div class="navbar-end">
-          <div class="field is-grouped">
-            <p class="control paragraph--padding-top">
-              <span class="title is-4" >GitHub stars history and charts</span>
-            </p>
-            <p class="control">
-              <a class="bd-tw-button button" target="_blank" href="https://github.com/pnowy/github-stars-history">
-              <span class="icon">
-                <b-icon pack="mdi" icon="github-circle" />
-              </span>
-              </a>
-            </p>
-            <p class="control paragraph--padding-top">
-              <a class="github-button" href="https://github.com/pnowy/github-stars-history" data-size="large" data-show-count="true" aria-label="Star pnowy/github-stars-history on GitHub">Star</a>
-            </p>
+          <span class="navbar-item is-4 has-text-weight-bold">
+            GitHub stars history and charts
+          </span>
+          <a class="navbar-item" href="https://github.com/pnowy/github-stars-history" target="_blank">
+            <span class="button">
+              <b-icon pack="mdi" icon="github-circle" />
+            </span>
+          </a>
+          <div class="navbar-item">
+            <div class="paragraph--padding-top">
+              <a class="github-button" href="https://github.com/pnowy/github-stars-history"
+                 data-size="large" data-show-count="true" aria-label="Star pnowy/github-stars-history on GitHub">Star</a>
+            </div>
           </div>
         </div>
       </div>
@@ -51,6 +50,6 @@ export default {
 
 <style scoped>
   .paragraph--padding-top {
-    padding-top: 5px;
+    padding-top: 6px;
   }
 </style>
