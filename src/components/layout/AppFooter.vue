285f18a6090bbbbcefe270d9ea7490b2ca1ce928
Przemek Nowak <przemek.nowak.pl@gmail.com>
2019-08-27 21:16:08 +0200

    Rename logo

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -2,18 +2,16 @@
   <footer class="footer">
     <div class="content has-text-centered">
       <p>
-        <strong>Github stars</strong> by <a href="https://github.com/pnowy">pnowy</a>
+        <strong>Github stars</strong> by <a href="https://github.com/pnowy" target="_blank">pnowy</a>
+      </p>
+      <p>Do you want add something to predefined stacks?
+        <a target="_blank" href="https://github.com/pnowy/github-stars-history/blob/master/src/data/stacks.js">
+          Just create pull request!
+        </a>
       </p>
       <p>
-        Do you want add something to predefined stacks? <a
-              target="_blank"
-              href="https://github.com/pnowy/github-stars-history/blob/master/src/data/stacks.js">Just create pull
-        request!</a>
-      </p>
-      <p>
-        Do you have an idea how to improve project? <a
-              target="_blank"
-              href="https://github.com/pnowy/github-stars-history/issues">Create an issue!</a>
+        Do you have an idea how to improve project?
+        <a target="_blank" href="https://github.com/pnowy/github-stars-history/issues">Create an issue!</a>
       </p>
     </div>
   </footer>
--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,24 +1,11 @@
 <template>
-  <nav
-          class="navbar is-light has-shadow is-spaced"
-          role="navigation"
-          aria-label="main navigation"
-  >
+  <nav class="navbar is-light has-shadow is-spaced" role="navigation" aria-label="main navigation">
+
     <div class="container">
       <div class="navbar-brand">
         <a class="navbar-item" href="#">
-          <img
-                  src="../../assets/logo_transparent_cut.png"
-                  alt="Github Stars History"
-                  class="logo"
-          />
+          <img src="../../assets/logo_transparent.png" alt="Github Stars History" class="logo"/>
         </a>
-
-        <!--<a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">-->
-        <!--<span aria-hidden="true"></span>-->
-        <!--<span aria-hidden="true"></span>-->
-        <!--<span aria-hidden="true"></span>-->
-        <!--</a>-->
       </div>
 
       <div class="navbar-menu">
@@ -26,25 +13,20 @@
           <span class="navbar-item is-4 has-text-weight-bold">
             GitHub stars history and charts
           </span>
-          <a
-                  class="navbar-item"
-                  href="https://github.com/pnowy/github-stars-history"
-                  target="_blank"
-          >
+          <a class="navbar-item" href="https://github.com/pnowy/github-stars-history" target="_blank">
             <span class="button">
               <b-icon icon="github" pack="fab"/>
             </span>
           </a>
           <div class="navbar-item">
             <div class="paragraph--padding-top">
-              <a
-                      class="github-button"
-                      href="https://github.com/pnowy/github-stars-history"
-                      data-size="large"
-                      data-show-count="true"
-                      aria-label="Star pnowy/github-stars-history on GitHub"
-              >Star</a
-              >
+              <a class="github-button"
+                 href="https://github.com/pnowy/github-stars-history"
+                 data-size="large"
+                 data-show-count="true"
+                 aria-label="Star pnowy/github-stars-history on GitHub">
+                Star
+              </a>
             </div>
           </div>
         </div>
