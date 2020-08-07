a9f0c0fc07e5194e412679be2ecd5e0f322cdb14
Przemek Nowak <przemek.nowak.pl@gmail.com>
2020-08-07 16:24:16 +0200

    prod-depl-friday

--- release.sh
+++ release.sh
@@ -18,17 +18,18 @@ if [ "$CURRENT_BRANCH" != "$APPROVED_BRANCH" ]; then
   exit
 fi
 
-# retrieve the last commit on the branch
+# retrieve tags data
 CURRENT_TAG=$(git describe --tags --match=v* --abbrev=0)
 CURRENT_TAG_TMS=$(git log -1 --format=%ai v0010)
-
 CURRENT_VERSION_NUMBER=${CURRENT_TAG#v}
 NEW_VERSION_VERSION_NUMBER=$(expr $CURRENT_VERSION_NUMBER + 1)
 NEW_TAG=$(printf "v%04d" $NEW_VERSION_VERSION_NUMBER)
+CURRENT_COMMIT_MSG=$(git log -1 --pretty=%B)
+CURRENT_TAG_COMMIT_MSG=$( git log -1 --pretty=%B $CURRENT_TAG)
 
 echo "Current branch:          $CURRENT_BRANCH"
-echo "Latest released Version: $CURRENT_TAG ($CURRENT_TAG_TMS)"
-echo "The next Version:        $NEW_TAG"
+echo "Latest released Version: $CURRENT_TAG ($CURRENT_TAG_COMMIT_MSG at $CURRENT_TAG_TMS)"
+echo "The next Version:        $NEW_TAG ($CURRENT_COMMIT_MSG)"
 echo
 read -p "Do you want to proceed? The tag will be created and pushed [y]? " -n 1 -r
 echo
